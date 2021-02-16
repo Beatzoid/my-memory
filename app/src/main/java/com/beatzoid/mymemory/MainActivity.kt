9587b27c79d0af833338ffd279717c3c08de0d99
Beatzoid <epiccoder7@gmail.com>
2021-02-16 09:06:38 -0800

    ✨ Add Game Logic
    Add the core game logic

--- app/src/main/java/com/beatzoid/mymemory/MainActivity.kt
+++ app/src/main/java/com/beatzoid/mymemory/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.beatzoid.mymemory.models.BoardSize
 import com.beatzoid.mymemory.models.MemoryCard
 import com.beatzoid.mymemory.models.MemoryGame
 import com.beatzoid.mymemory.utils.DEFAULT_ICONS
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,28 +20,48 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var rvBoard: RecyclerView
+    private lateinit var clRoot: ConstraintLayout
     private lateinit var tvNumMoves: TextView
     private lateinit var tvNumPairs: TextView
 
+    private lateinit var memoryGame: MemoryGame
+    private lateinit var adapter: MemoryBoardAdapter
     private var boardSize: BoardSize = BoardSize.EASY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clRoot = findViewById(R.id.clRoot)
         rvBoard = findViewById(R.id.rvBoard)
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
 
-       val memoryGame = MemoryGame(boardSize)
+       memoryGame = MemoryGame(boardSize)
 
-        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener {
+        adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener {
             override fun onCardClick(position: Int) {
-                Log.i(TAG, "Card clicked $position")
+                updateGameWithFlip(position)
             }
 
         })
+        rvBoard.adapter = adapter
+
         rvBoard.setHasFixedSize(true)
         rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+
+    private fun updateGameWithFlip(position: Int) {
+        if (memoryGame.haveWonGame()) {
+            Snackbar.make(clRoot, "You already won!", Snackbar.LENGTH_LONG).show()
+            return
+        }
+        if (memoryGame.isCardFaceUp(position)) {
+            Snackbar.make(clRoot, "Invalid move!", Snackbar.LENGTH_LONG).show()
+            return
+        }
+
+        memoryGame.flipCard(position)
+        adapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/beatzoid/mymemory/models/MemoryGame.kt
+++ app/src/main/java/com/beatzoid/mymemory/models/MemoryGame.kt
@@ -3,13 +3,56 @@ package com.beatzoid.mymemory.models
 import com.beatzoid.mymemory.utils.DEFAULT_ICONS
 
 class MemoryGame (private val boardSize: BoardSize) {
-
     val cards: List<MemoryCard>
-    val numPairsFounds = 0
+    var numPairsFounds = 0
+
+    private var indexOfSingleSelectedCard: Int? = null
 
     init {
         val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs())
         val randomizedImages = (chosenImages + chosenImages).shuffled()
         cards = randomizedImages.map { MemoryCard(it) }
+    }
+
+    fun flipCard(position: Int): Boolean {
+        val card = cards[position]
+        var foundMatch = false
+        if (indexOfSingleSelectedCard == null) {
+            // 0 or 2 cards previously flipped over
+            restoreCards()
+            indexOfSingleSelectedCard = position
+        } else {
+            // Exactly 1 card previously flipped over
+            foundMatch = checkForMatch(indexOfSingleSelectedCard!!, position)
+            indexOfSingleSelectedCard = null
+        }
+        card.isFaceUp = !card.isFaceUp
+        return foundMatch
+    }
+
+    private fun checkForMatch(position1: Int, position2: Int): Boolean {
+        if (cards[position1].identifier != cards[position2].identifier) {
+            return false
+        }
+        cards[position1].isMatched = true
+        cards[position2].isMatched = true
+        numPairsFounds++
+        return true
+    }
+
+    private fun restoreCards() {
+        for (card in cards) {
+            if (!card.isMatched) {
+                card.isFaceUp = false
+            }
+        }
+    }
+
+    fun haveWonGame(): Boolean {
+        return numPairsFounds == boardSize.getNumPairs()
+    }
+
+    fun isCardFaceUp(position: Int): Boolean {
+        return cards[position].isFaceUp
     }
 }
