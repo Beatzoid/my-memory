2e22a231f4d81335ad12eec34348c7f6ec2a006c
Beatzoid <epiccoder7@gmail.com>
2021-02-17 09:17:19 -0800

    ✨ Add Image Upload Ability
    Add the ability to upload images to the custom game creation menu

--- app/src/main/java/com/beatzoid/mymemory/ImagePickerAdapter.kt
+++ app/src/main/java/com/beatzoid/mymemory/ImagePickerAdapter.kt
@@ -13,8 +13,13 @@ import kotlin.math.min
 class ImagePickerAdapter(
     private val context: Context,
     private val imageUris: List<Uri>,
-    private val boardSize: BoardSize
+    private val boardSize: BoardSize,
+    private val imageClickListener: ImageClickListener
 ) : RecyclerView.Adapter<ImagePickerAdapter.ViewHolder>() {
+
+    interface ImageClickListener {
+        fun onPlaceholderClicked()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.card_image, parent, false)
@@ -42,12 +47,12 @@ class ImagePickerAdapter(
 
         fun bind(uri: Uri) {
             ivCustomImage.setImageURI(uri)
-            ivCustomImage.setOnClickListener(null)
+            // ivCustomImage.setOnClickListener(null)
         }
 
         fun bind() {
             ivCustomImage.setOnClickListener {
-                // Launch intent for user to select photos
+               imageClickListener.onPlaceholderClicked()
             }
         }
     }
